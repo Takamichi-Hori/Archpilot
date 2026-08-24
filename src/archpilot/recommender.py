efacf473e2df8752155a008a65e61f8baec810d1
Takamichi Hori <takamichihori9@gmail.com>
2026-08-24 23:32:13 +0900

    add some packages on recommender.py

---
+++ src/archpilot/recommender.py
@@ -0,0 +1,23 @@
+from archpilot.models import Recommendation, SystemInfo
+
+BASE_PACKAGES = [
+    "base",
+    "linux",
+    "linux-firmware",
+    "networkmanager",
+    "pipewire",
+    "pipewire-pulse",
+]
+
+KDE_PACKAGES = [
+    "plasma-meta",
+    "konsole",
+    "dolphin",
+]
+
+GAMING_PACKAGES = [
+    "steam",
+    "gamemode",
+    "gamescope",
+    "mangohud",
+]
