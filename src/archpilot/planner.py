78c6cbcc5aab06c619f348850ce5e697cd15b80f
Takamichi Hori <takamichihori9@gmail.com>
2026-08-25 16:39:11 +0900

    add  in the basic install plan

---
+++ src/archpilot/planner.py
@@ -0,0 +1,8 @@
+from archpilot.models import InstallPlan, Recommendation
+
+def create_install_plan (recommendation: Recommendation,) -> InstallPlan:
+
+    return InstallPlan(
+        recommendation=recommendation,
+        destructive_actions_allowed=False,
+    )
